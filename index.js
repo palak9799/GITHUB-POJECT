0d71a9a99dd788a9849537b5a4c127fd1d0d4778
palak9799 <palakshrimali300@gmail.com>
2026-05-06 21:34:51 +0530

    cancelAnimationFrame

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ let score = 0;
 score++; // increases score to 1
 score += 5; // increases score to 6
 
-console.log(`${name} has a score of ${score}.`); // Output: Alice has a score of 6.
+console.log(`${name} has a score of ${score}.`); // Output: Alice has a score of 6.0.
+cancelAnimationFrame
+document.getElementById("myTitle").textContent = "New Title!";
+function sayHello() {
+    console.log("Hello, world!");
+}
+sayHello(); // Runs the function
+// Logging a simple string
+console.log("Hello, World!");
+
+// Logging a variable
+let score = 100;
+console.log(score); 
+
+
