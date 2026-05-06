3e3b137497d05bf24304147b87404c9de569aa60
palak9799 <palakshrimali300@gmail.com>
2026-05-06 21:26:03 +0530

    new file index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const name = "Alice";
+let score = 0;
+score++; // increases score to 1
+score += 5; // increases score to 6
+
+console.log(`${name} has a score of ${score}.`); // Output: Alice has a score of 6.
